f12579165659c41078475ca3d53d7604cd3597a3
Brendan Armstrong <brendanarmstrong@proton.me>
2024-04-08 20:20:18 -0400

    ref: formatting

--- lua/lua_snippet_repo/init.lua
+++ lua/lua_snippet_repo/init.lua
@@ -2,7 +2,6 @@ local M = {}
 
 local luasnip_snippets_path = "lua/lua_snippet_repo/snippets"
 
-
 function M.load_snippets()
   return vim.api.nvim_get_runtime_file(luasnip_snippets_path, true)
 end
