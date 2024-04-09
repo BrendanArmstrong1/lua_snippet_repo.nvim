8ebd98962c8b925cc0e24103c2a203314f97e88e
Brendan Armstrong <brendanarmstrong@proton.me>
2024-04-08 20:20:25 -0400

    added lua snippets(snippet imports, and string replace)

--- lua/lua_snippet_repo/snippets/lua.lua
+++ lua/lua_snippet_repo/snippets/lua.lua
@@ -16,9 +16,9 @@ local m = require("luasnip.extras").match
 local n = require("luasnip.extras").nonempty
 local dl = require("luasnip.extras").dynamic_lambda
 local fmta = require("luasnip.extras.fmt").fmta
-local types = require("luasnip.util.types")
 local conds = require("luasnip.extras.conditions")
 local conds_expand = require("luasnip.extras.conditions.expand")
+local types = require("luasnip.util.types")
 
 return {
   s(
@@ -31,4 +31,49 @@ return {
       i(1),
     })
   ),
+  s(
+    "escaped_text",
+    fmta(
+      [[
+local escaped_characters = {
+  ["%("] = "\\(",
+  ["%)"] = "\\)",
+  ["%{"] = "\\{",
+  ["%}"] = "\\}",
+}
+local escaped_text = <text>
+for i, v in pairs(escaped_characters) do
+  escaped_text = string.gsub(escaped_text, i, v)
+end
+<>
+  ]],
+      {
+        text = i(1, "target_string"),
+        i(2,""),
+      }
+    )
+  ),
+  s("snippet_tags",fmt([[
+local ls = require("luasnip")
+local re = require("luasnip.extras").rep
+local fmt = require("luasnip.extras.fmt").fmt
+local i = ls.insert_node
+local s = ls.snippet
+local t = ls.text_node
+local sn = ls.snippet_node
+local f = ls.function_node
+local c = ls.choice_node
+local d = ls.dynamic_node
+local r = ls.restore_node
+local l = require("luasnip.extras").lambda
+local rep = require("luasnip.extras").rep
+local p = require("luasnip.extras").partial
+local m = require("luasnip.extras").match
+local n = require("luasnip.extras").nonempty
+local dl = require("luasnip.extras").dynamic_lambda
+local fmta = require("luasnip.extras.fmt").fmta
+local conds = require("luasnip.extras.conditions")
+local conds_expand = require("luasnip.extras.conditions.expand")
+local types = require("luasnip.util.types")
+  ]],{})),
 }
