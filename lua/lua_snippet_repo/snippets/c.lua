dc0c40ae5e5bfee39ed179319e492517aa29a9f2
Brendan Armstrong <brendanarmstrong@proton.me>
2024-10-20 06:39:45 -0400

    feat: add fileopening snippet to C

--- lua/lua_snippet_repo/snippets/c.lua
+++ lua/lua_snippet_repo/snippets/c.lua
@@ -1,6 +1,5 @@
 local stype = require("lua_snippet_repo.snippets.snippet_types")
 
-
 return {
   stype.s(
     "print",
@@ -8,5 +7,19 @@ return {
       s = stype.i(1, "s"),
       a = stype.i(2, ""),
     })
+  ),
+  stype.s(
+    "sn_fileopen",
+    stype.fmt(
+      [[
+  FILE *fptr;
+  char  mystring[100];
+
+  fptr = fopen("{filename}", "r");
+  fgets(mystring, 100, fptr);
+
+  ]],
+      { filename = stype.i(1, "filename.txt") }
+    )
   ),
 }
